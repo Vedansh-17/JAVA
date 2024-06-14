bb486facb1438419253e98726143522b70fa5477
Vedansh-17 <160649508+Vedansh-17@users.noreply.github.com>
2024-06-14 19:57:24 -0400

    TriangleArea
    Java code for finding the area of a triangle with the prefixed values.

---
+++ main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+      float a = area(1.4f, 2.5f);
+      System.out.print(a);
+    }
+
+    public  static float area(float h, float w){
+        {
+            float Height, Width, Area;
+            Height = 2.4f;
+            Width =1.7f;
+            Area = Height * Width;
+            return Area;
+        }
+    }
+}
